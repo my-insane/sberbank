8e22b9bb2e2538d67ff56e503d93d83e79108d6d
vld <nbaitru@gmail.com>
2020-06-09 10:10:14 +0300

    Complete s2001 for SGU

---
+++ src/sgu/s2001.java
@@ -0,0 +1,16 @@
+package sgu;
+
+import java.util.*;
+import java.io.*;
+
+public class s2001 {
+    public static void main(String[] argv) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(in.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = a + b;
+
+        System.out.println(c);
+    }
+}
