610efe88739b638e55002ade0532cbf8a906c970
vld <nbaitru@gmail.com>
2020-06-11 10:52:26 +0300

    Change doc in Person.marry

--- src/hw2/Person.java
+++ src/hw2/Person.java
@@ -13,7 +13,7 @@ public class Person {
     /**
      *      * This method checks gender of persons. If genders are not equal - tries to marry.
      *      * If one of them has another spouse - execute divorce(sets spouse = null for husband and wife. Example: if both persons have spouses - then divorce will set 4 spouse to null) and then executes marry().
-     *      * @param person - new husband/wife for this person.
+     *      * @spouse person - new husband/wife for this person.
      *      * @return - returns true if this person has another gender than passed person and they are not husband and wife, false otherwise
      *      
      */
