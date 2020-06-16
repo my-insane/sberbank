e2642c8e6ad624f6a72789917cb0f11dd542c2d9
vld <nbaitru@gmail.com>
2020-06-16 11:44:42 +0300

    Add s2057 for SGU But this is don't complete

--- src/sgu/s2057.java
+++ src/sgu/s2057.java
@@ -1,4 +1,39 @@
 package sgu;
 
+import java.util.*;
+import java.io.*;
+
 public class s2057 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        HashMap<Integer, Integer> fr = new HashMap<>();
+        int trigger;
+        int num = 0;
+        StringTokenizer st = new StringTokenizer(in.readLine());
+        final int N = Integer.parseInt(st.nextToken());
+        for (int i = 1; i <= N; i++) {
+            StringTokenizer st2 = new StringTokenizer(in.readLine());
+            trigger = Integer.parseInt(st2.nextToken());
+            if (trigger == 1) {
+                num = Integer.parseInt(st2.nextToken());
+                if (fr.get(num) != null) {
+                    if (fr.containsKey(num)) {
+                        int key = fr.get(num) + 1;
+                        fr.replace(num, key);
+                    }
+                } else fr.put(num, 1);
+            } else {
+                int firstKey = fr.entrySet().stream().min((a, b) -> a.getValue().compareTo(b.getValue())).get().getKey();
+                System.out.println(fr.get(firstKey));
+                int key = fr.get(num) - 1;
+                if (key == 0) {
+                    fr.remove(num);
+                }
+            }
+        }
+    }
+
 }
+
+
+
